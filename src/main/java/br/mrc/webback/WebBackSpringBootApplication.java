565f439c83f1fc778aa6244e5141b5e579a7b005
MRC <miguel826@hotmail.com.br>
2022-05-14 08:41:07 -0300

    Project created

---
+++ src/main/java/br/mrc/webback/WebBackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.mrc.webback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBackSpringBootApplication.class, args);
+	}
+
+}
